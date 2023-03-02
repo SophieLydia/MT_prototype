5f32404aa6cc7f7e8887bb94a40a0d5c15fc4a47
Sophie Crevoiserat <sophie.crevoiserat@unifr.ch>
2023-03-02 11:26:20 +0100

    add info message for each step

--- js/components/Scenario.js
+++ js/components/Scenario.js
@@ -6,6 +6,7 @@ class Scenario {
     currentStep;
     imageLoad;
     scene;
+    doc;
 
     constructor(scene){
         this.scene = scene;
@@ -14,11 +15,13 @@ class Scenario {
         // Also load initial state
         this.imageLoad = new ImageLoad(scene);
 
+        this.doc = document.getElementById("infoStep");
+
         document.getElementById("nextStep").addEventListener("click", this.getStep.bind(this));
+        this.doc.appendChild(document.createTextNode("Starting scenario 1"));
     }
 
     getStep(){
-        console.log(this.scene.children)
         this.currentStep += 1;
         switch(this.currentStep){
             // Initial state, scanning a marker doesn't change anything
@@ -28,10 +31,14 @@ class Scenario {
                 break;
             // Step 1: check PC turn on
             case 1:
+                this.doc.removeChild(this.doc.lastChild);
+                this.doc.appendChild(document.createTextNode("Check that the PC is turned on"));
                 this.imageLoad.replaceMesh("./images/physicalComponents/PC_currentStep.svg", "PC");
                 break;
             // Step 2: check power cables connected
             case 2:
+                this.doc.removeChild(this.doc.lastChild);
+                this.doc.appendChild(document.createTextNode("Check that the power cable is connected on the PC and the wall"));
                 this.imageLoad.replaceMesh("./images/physicalComponents/PC.svg", "PC");
                 this.imageLoad.replaceMesh("./images/physicalComponents/cable2_currentStep.svg", "PC");
                 this.imageLoad.replaceMesh("./images/physicalComponents/powerPort_currentStep.svg", "PC");
@@ -39,15 +46,21 @@ class Scenario {
                 break;
             // Step 3: check power ports working
             case 3:
-                this.imageLoad.replaceMesh("./images/physicalComponents/powerPort.svg", "wall", 1);
+                this.doc.removeChild(this.doc.lastChild);
+                this.doc.appendChild(document.createTextNode("Check that power ports on the PC and the wall are working"));
                 this.imageLoad.replaceMesh("./images/physicalComponents/cable2.svg", "PC");
                 break;
             // Step 4: check power cable working
             case 4:
+                this.doc.removeChild(this.doc.lastChild);
+                this.doc.appendChild(document.createTextNode("Check that the power cable between the PC and the wall is working"));
                 this.imageLoad.replaceMesh("./images/physicalComponents/powerPort.svg", "PC");
+                this.imageLoad.replaceMesh("./images/physicalComponents/powerPort.svg", "wall", 1);
                 this.imageLoad.replaceMesh("./images/physicalComponents/cable2_currentStep.svg", "PC");
                 break;
             default:
+                this.doc.removeChild(this.doc.lastChild);
+                this.doc.appendChild(document.createTextNode("The scenario is finished"));
                 this.imageLoad.replaceMesh("./images/physicalComponents/cable2.svg", "PC");
                 console.log("The scenario is finished");
         }
